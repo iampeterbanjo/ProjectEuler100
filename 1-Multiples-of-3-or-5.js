a3090654b4b21573c9c0be98cc3ef60e687c55cf
iampeterbanjo <me@iampeterbanjo.com>
2024-03-24 21:59:39 +0000

    multiples of 3 or 5

---
+++ 1-Multiples-of-3-or-5.js
@@ -0,0 +1,21 @@
+/**
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+ */
+function multiplesOf3Or5(number) {
+    // check number is multiple of 3 or 5
+    const isValidMultiple = (number) => number % 3 === 0 || number % 5 === 0
+    
+    let sum = 0;
+    let count = 0;
+    while (count < number) {
+      if (isValidMultiple(count)) {
+        sum += count;
+      }
+      count++;
+    }
+    return sum;
+  }
+  
+  multiplesOf3Or5(1000);
---
+++ 1-Multiples-of-3-or-5.spec.js
@@ -0,0 +1,24 @@
+import { multiplesOf3Or5 } from './1-Multiples-of-3-or-5';
+
+describe("Multiples of 3 or 5", () => {
+    test("multiplesOf3Or5(10) should return a number.", () => {
+        const result = multiplesOf3Or5(10);
+        assert(multiplesOf3Or5(10)).equals(isNumber(10))
+    });
+    test("multiplesOf3Or5(49) should return 543.", () => {
+        const result = multiplesOf3Or5(49);
+        assert(multiplesOf3Or5(10)).equals(isNumber(543))
+    });
+    test("multiplesOf3Or5(1000) should return 233168.", () => {
+        const result = multiplesOf3Or5(1000);
+        assert(result).equals(233168)
+    });
+    test("multiplesOf3Or5(8456) should return 16687353.", () => {
+        const result = multiplesOf3Or5(1000);
+        assert(result).equals(16687353)
+    });
+    test("multiplesOf3Or5(19564) should return 89301183.", () => {
+        const result = multiplesOf3Or5(1000);
+        assert(result).equals(89301183)
+    });
+})
