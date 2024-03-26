77bdc8dafb435dac7ffd2cdff65cc86329ba5e48
iampeterbanjo <me@iampeterbanjo.com>
2024-03-26 22:42:29 +0000

    feat(solutions): largest prime factor

---
+++ 3-largest-prime-factor.js
@@ -0,0 +1,33 @@
+/**
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the given number?
+ */
+
+function largestPrimeFactor(number) {
+    // check if a number is prime
+    const isPrime = (maybe) => {
+        if (maybe < 2) return false;
+        for (let i = 2; i <= Math.sqrt(maybe); i++) {
+        if (maybe % i === 0) {
+            return false;
+        }
+        }
+        return true;
+    };
+
+    let largestPrime = 1;
+    let currentFactor = 2;
+    let remainingNumber = number;
+
+    while (remainingNumber > 1) {
+        if (remainingNumber % currentFactor === 0) {
+        largestPrime = currentFactor;
+        remainingNumber /= currentFactor;
+        } else {
+        currentFactor++;
+        }
+    }
+
+    return largestPrime;
+}
